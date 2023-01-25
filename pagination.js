52f777193a85f81a27e67bfa1b5a109631e0b686
tanukili <108317269+tanukili@users.noreply.github.com>
2023-01-25 22:34:11 +0800

    分頁功能

--- pagination.js
+++ pagination.js
@@ -1,18 +1,29 @@
 export default {
     // prop 將外層資料傳入內層
-    props: ['page'],
+    props: ['pages', 'getData'],
     template: `<nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+      <li class="page-item"
+        :class="{ disabled: !pages.has_pre }">
+        <a class="page-link" href="#" aria-label="Previous"
+          @click.prevent="getData(pages.current_page - 1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+      <!-- 呈現頁數 -->
+      <li class="page-item" 
+        v-for="page in pages.total_pages" :key="page +'page'"
+        :class="{ active: pages.current_page === page }">
+        <!-- 記得要把 page 作為參數帶入 -->
+        <a class="page-link" href="#"
+          @click.prevent="getData(page)">
+          {{ page }}
+        </a>
+      </li>
+      <li class="page-item"
+        :class="{ disabled: !pages.has_next }">
+        <a class="page-link" href="#" aria-label="Next"
+          @click.prevent="getData(pages.current_page + 1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
